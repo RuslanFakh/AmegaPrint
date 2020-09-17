6ca93f2de92858b939c0548ec7fb3f084de18047
Ruslan <rusel1362@gmail.com>
2020-09-17 21:55:03 +0500

    7th

--- src/js/contact_page.js
+++ src/js/contact_page.js
@@ -34,3 +34,49 @@ btn_close.onclick = function(e){
   document.body.style.overflow = ''
 	mobile_block.style.display = 'none'
 }
+
+document.querySelectorAll('.select').forEach(select => { //Выбриаем все выпадающие списки на странице
+
+	let selectCurrent = select.querySelector('.select__current'),
+			selectList = select.querySelector('.select__list'),
+			selectInput = select.querySelector('.select__input'),
+			selectItem = select.querySelectorAll('.select__item');
+
+	//по клику добавляем/удалям класс
+	selectCurrent.addEventListener('click', () => {
+		selectList.classList.toggle('select__list--show')
+	})
+
+	//обходим элементы списка
+	selectItem.forEach(item =>{
+
+		//обрабатываем событие клик по элементу
+		item.addEventListener('click', ()=>{
+
+			//получаем значение из data-атрибута
+			let itemValue = item.getAttribute('data-value')
+
+			//получаем содержание элемента (текст)
+			let itemText = item.textContent
+
+			//присваиваем инпуту ранее полученное значение из data-атрибута
+			selectInput.value = itemValue
+
+			//присваиваем текущее значение (текст)
+			selectCurrent.textContent = itemText
+
+			//скрываем выпадающий список
+			selectListHide()
+		})
+	})
+
+	// функция закрытия выпадающего списка
+	let selectListHide = () => {
+		selectList.classList.remove('select__list--show')
+	}
+	//Закрываем выпадающий сисок, если клик был вне области
+	document.addEventListener('mouseup', (e) =>{
+    if (!select.contains(e.target))	selectListHide()
+  })
+
+})
